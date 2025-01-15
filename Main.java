e0b01bbefce52d639e0d7280d69bcb235bd91786
Rayman The Legend <iafrate.samuele@itiscannizzaro.edu.it>
2025-01-15 12:24:22 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,51 +1,66 @@
-// Esercizio: Lavorare con le Matrici in Java
+//Esercizio: Lavorare con le Matrici in Java
 //
-// 1. Creare una matrice 3x3 di numeri interi.
-// 2. Popolare la matrice con valori casuali compresi tra 1 e 100.
-// 3. Implementare un metodo che calcoli la somma di tutti gli elementi della matrice.
-// 4. Implementare un metodo che trovi il valore massimo nella matrice.
-// 5. Stampare la matrice, la somma degli elementi e il valore massimo trovato.
+//1. Creare una matrice 3x3 di numeri interi.
+//2. Popolare la matrice con valori casuali compresi tra 1 e 100.
+//3. Implementare un metodo che calcoli la somma di tutti gli elementi della matrice.
+//4. Implementare un metodo che trovi il valore massimo nella matrice.
+//5. Stampare la matrice, la somma degli elementi e il valore massimo trovato.
 //
-// Suggerimento:
-// - Utilizza la classe Random per generare numeri casuali.
-// - Per stampare la matrice, usa un ciclo annidato.
+//Suggerimento:
+//- Utilizza la classe Random per generare numeri casuali.
+//- Per stampare la matrice, usa un ciclo annidato.
+import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
-        // Creazione e popolamento della matrice
-        int[][] matrix = new int[3][3];
-        Random random = new Random();
-        // COMPLETARE IL CODICE
+public class bo {
+ public static void main(String[] args) {
+     // Creazione e popolamento della matrice
+     int[][] matrix = new int[3][3];
+     Random random = new Random();
+     //INSERISCI IL CODICE
+     for(int cont = 0; cont < matrix.length; cont++) {
+    	 for(int c = 0; c < matrix.length; c++) {
+    		 matrix[cont][c] =(int) (Math.random() * 100 + 1);
+    	 }
+     }   
 
-      
+     // Stampa della matrice
+     System.out.println("Matrice:");
+     // COMPLETARE IL CODICE
+     System.out.println(Arrays.toString(matrix[0])+"\n"
+     		           +Arrays.toString(matrix[1])+"\n"
+     		           +Arrays.toString(matrix[2]));
 
-        // Stampa della matrice
-        System.out.println("Matrice:");
-        // COMPLETARE IL CODICE
 
+     // Calcolo della somma degli elementi
+     int sum = calculateSum(matrix);
+     System.out.println("Somma degli elementi: " + sum);
 
-        // Calcolo della somma degli elementi
-        int sum = calculateSum(matrix);
-        System.out.println("Somma degli elementi: " + sum);
+     // Ricerca del valore massimo
+     int max = findMax(matrix);
+     System.out.println("Valore massimo: " + max);
+ }
 
-        // Ricerca del valore massimo
-        int max = findMax(matrix);
-        System.out.println("Valore massimo: " + max);
-    }
+ public static int calculateSum(int[][] matrix) {
+     int sum = 0;
 
-    public static int calculateSum(int[][] matrix) {
-        int sum = 0;
-   
-// COMPLETARE IL CODICE
-      
-        return sum;
-    }
+//COMPLETARE IL CODICE
+     for(int cont = 0; cont < matrix.length; cont++) {
+    	 for(int c = 0; c < matrix.length; c++) {
+    		 sum += matrix[c][cont];
+    	 }
+     }
+     return sum;
+ }
 
-    public static int findMax(int[][] matrix) {
-        int max = matrix[0][0];
+ public static int findMax(int[][] matrix) {
+     int max = matrix[0][0];
 
-// COMPLETARE IL CODICE
-      
-        return max;
-    }
+//COMPLETARE IL CODICE
+     for(int cont = 0; cont < matrix.length; cont++) {
+    	 for(int c = 0; c < matrix.length; c++) {
+    		 max = Math.max(max, matrix[c][cont]);
+    	 }
+     }   
+     return max;
+ }
 }
